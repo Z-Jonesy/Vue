9e3b64fcd0614454d5c0d32f5663f904e3ff4722
Z <zoltan_bogdan@live.com>
2018-06-05 19:17:21 +0200

    01_init

---
+++ 01_osztalynaplo/scripts.js
@@ -0,0 +1,16 @@
+console.log("init");
+
+let app = new Vue({
+    el: 'main#app',
+    data: {
+        pageTitle: 'Osztálynapló',
+        studentCollection: [
+            'Anna',
+            'Béla',
+            'Tomi',
+            'András',
+            'Panka',
+            'Dóri'
+        ]
+    }
+});
