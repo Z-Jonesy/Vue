db3d6814805f2a02d42d7768322a697b933ee6d1
Zé <zoltan.bogdan@gmail.com>
2018-06-05 20:34:12 +0200

    added 02_kereso

---
+++ 02_kereso/scripts-vue.js
@@ -0,0 +1,42 @@
+let app = new Vue({
+    el: 'main#app',
+    data: {
+        nameFilter: '',
+        filmCollection: []
+    },
+    created() {
+        this.filmCollection = [
+            'Raging bull',
+            'Aliens',
+            'Reservoir dogs',
+            'Wall - e',
+            'The tresor of sierra madre',
+            'Whiplash',
+            'Some like it hot',
+            'Double indemnity',
+            'Taxi driver',
+            'Vertigo',
+            'On the waterfront',
+            'Saving private ryan',
+            'Inception',
+            'The lord of the rings',
+            'The silence of the lambs'
+        ];
+        this.filmCollection.sort(function(a,b){
+            return a.localCompare(b);
+        })
+    },
+    methods: {
+        GetFilteredList(){
+            return list.filmCollection.filter(item => {
+                if (this.nameFilter == '') {
+                    return true;
+                }
+
+                //pozíció megállapítása
+                let position = item.toLowerCase().indexOf(this.nameFilter.toLowerCase());
+                return position != -1;
+            });
+        }
+    }
+});
