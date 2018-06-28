bf2a4419618817476991182f4c252a8279eeb72a
Z Jonesy <zoltan_bogdan@live.com>
2018-06-28 07:47:19 +0200

    04_webpack_adding_counterButton_v21

--- 04_webpack/src/App.vue
+++ 04_webpack/src/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         {{message}}
+        <div>
+            <CounterButton></CounterButton>
+        </div>
     </div>
 </template>
 
-
 <script>
+import CounterButton from './CounterButton.vue';
 export default {
+    components: { CounterButton },
     data: function() {
         return {
             message: 'Sikerült a vue behúzás'
---
+++ 04_webpack/src/CounterButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="Increase()" v-bind:class="classes">{{ counter }}</button>
+    
+</template>
+
+<script>
+export default {
+    props: ["theme", "offset"],
+    data: function(){
+        return {
+            counter: 0
+        };
+    },
+    methods: {
+        Increase(){
+            this.counter++;
+        }
+    },
+    computed:{
+        classes(){
+            return this.counter + this.offset
+        }
+    }
+};
+</script>
