d04488d7fed8083384e92cbc307fcb0be06189d8
Z Jonesy <zoltan_bogdan@live.com>
2018-06-29 12:24:15 +0200

    fizuhu_refactor_added_Navigation and Footer components

---
+++ fizuhu/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    <Navigation></Navigation>
+    <div>{{ message }}</div>
+    <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+    components:{
+        Navigation,
+        Footer
+    },
+    data: function(){
+        return {message: 'OK'}
+    }
+}
+</script>
